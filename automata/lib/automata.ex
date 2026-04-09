25991fb774339b73bd4ea0fdb6c2c6f0482e8dd5
aless2301 <alessandra.caballero01@gmail.com>
2026-04-08 20:39:04 -0600

    Determinization algorithm for a non-deterministic finite automaton without epsilon transitions

--- automata/lib/automata.ex
+++ automata/lib/automata.ex
@@ -17,9 +17,44 @@ defmodule Automata do
       accept_states: MapSet.new([3])
     }
   end
-"""
-  def determinize do
-
+  # Q' = P(Q) powerset
+  defp powerset([]), do: [MapSet.new()] #caso base
+  defp powerset([h | t]) do
+    sub = powerset(t)
+    sub ++ Enum.map(sub, fn s -> MapSet.put(s, h) end)
   end
-"""
+
+  def determinize(nfa) do
+    all_states = powerset( MapSet.to_list(nfa.states))
+    # Si el subconjunto y los accepted states del NFA tienen algo en común
+    dfa_accept_states = Enum.filter(all_states, fn subset -> !MapSet.disjoint?(subset, nfa.accept_states) end)
+    # 2. Para cada estado (subset), iteramos sobre cada letra del alfabeto (:a, :b)
+
+    dfa_transitions = Enum.reduce(all_states, %{}, fn subset, acc ->
+    Enum.reduce(nfa.alphabet, acc, fn symbol, acc2 ->
+      dest = Enum.reduce(subset, MapSet.new(), fn state, union ->
+        nfa_dest = Map.get(nfa.transitions, {state, symbol}, MapSet.new())
+        MapSet.union(union, nfa_dest)
+      end)
+      # Guardamos la transición: {Subconjunto, Símbolo} -> Subconjunto Destino
+        Map.put(acc2, {subset, symbol}, dest)
+      end)
+    end)
+
+  #dfa
+
+    %{
+      states: MapSet.new(all_states),
+      alphabet: nfa.alphabet,
+      transitions: dfa_transitions,
+
+      #%{
+        #idk yet
+
+      #},
+      start_state: MapSet.new([nfa.start_state]),
+      accept_states: MapSet.new(dfa_accept_states)
+    }
+  end
+
 end
--- automata/test/automata_test.exs
+++ automata/test/automata_test.exs
@@ -2,7 +2,25 @@ defmodule AutomataTest do
   use ExUnit.Case
   doctest Automata
 
-  test "greets the world" do
-    assert Automata.hello() == :world
+  test "correct number of states" do
+    nfa = Automata.nfa()
+    dfa = Automata.determinize(nfa)
+    assert MapSet.size(dfa.states) == 16
   end
+
+   test "initial state {0}" do
+    nfa = Automata.nfa()
+    dfa = Automata.determinize(nfa)
+    assert dfa.start_state == MapSet.new([0])
+  end
+
+  test "abb accepted" do
+    dfa = Automata.nfa() |> Automata.determinize()
+    # Simulamos: {0} -a-> {0,1} -b-> {0,2} -b-> {0,3} -b-> {0,3}
+    s0 = Map.get(dfa.transitions, {MapSet.new([0]),       :a})
+    s1 = Map.get(dfa.transitions, {s0,                    :b})
+    s2 = Map.get(dfa.transitions, {s1,                    :b})
+    assert MapSet.member?(dfa.accept_states, s2)
+  end
+
 end
