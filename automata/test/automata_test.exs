58596f540f9685e07662b238b63b0fdba8990bfb
aless2301 <alessandra.caballero01@gmail.com>
2026-04-17 23:36:18 -0600

    Including test cases for e_determinize().

--- automata/test/automata_test.exs
+++ automata/test/automata_test.exs
@@ -23,4 +23,30 @@ defmodule AutomataTest do
     assert MapSet.member?(dfa.accept_states, s2)
   end
 
+  test "start state e_nfa to dfa correct" do
+    nfa = Automata.e_nfa()
+
+    dfa = Automata.e_determinize(nfa)
+
+    expected_start = MapSet.new([0, 1, 2, 3, 7])
+    assert dfa.start_state == expected_start
+
+  end
+
+  test "same alphabet for dfa and e_nfa" do
+    enfa = Automata.e_nfa()
+    dfa = Automata.determinize(enfa)
+    assert MapSet.equal?(dfa.alphabet, enfa.alphabet)
+  end
+
+  test "correct final states" do
+    enfa = Automata.e_nfa()
+    dfa = Automata.determinize(enfa)
+
+    refute MapSet.equal?(dfa.accept_states, MapSet.new())
+
+    assert Enum.any?(dfa.accept_states, fn state_set ->
+      MapSet.member?(state_set, 10)
+    end)
+  end
 end
